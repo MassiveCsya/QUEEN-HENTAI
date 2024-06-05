fbc6295fe4601b4bd284b582e9c56477947c66ec
MassiveCsya <juniqcsya@gmail.com>
2024-06-05 16:33:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -60,7 +60,7 @@ global.fglog = 'https://i.ibb.co/T1DqdYz/thumbnail.jpg'
 
 
 global.wait = '*💝 _Waiting Bro Uploading by 💝 Queen Hentai 💝..._*'
-global.rwait = '🇱🇰'
+global.rwait = '🌐'
 global.dmoji = '🤣'
 global.done = '👍'
 global.error = '👎' 
